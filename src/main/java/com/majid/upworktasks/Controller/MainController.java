bda035d8b44bf1b95124684da4e8a2d59807f0aa
Majid Paplu <majid.hussainqutriyo@gmail.com>
2023-04-05 21:47:01 +0500

    commit

--- src/main/java/com/majid/upworktasks/Controller/MainController.java
+++ src/main/java/com/majid/upworktasks/Controller/MainController.java
@@ -10,11 +10,16 @@ public class MainController {
     /**
      * @return
      */
+    // when we call /my-api on browser so you will get same data as you given in your api..!  
     @GetMapping("/my-api")
     public String about(){
 		RestTemplate restTemplate = new RestTemplate();
         String url = "https://taaskserver.herokuapp.com/api/user/getAllUsers";
         String result = restTemplate.getForObject(url, String.class);
+
+        // This will print in console screen..!
+        System.out.println(result);
+
         return result;
     }
 
